040334e48fca56c7509e61637f427fbb79ac9f3d
Ruchitsoni1898 <ruchitsoni1898@gmail.com>
2023-09-10 21:57:59 +0200

    Initial commit

---
+++ Madlibs.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+# Questions for the user to answer
+noun = input("choose the noun:")
+p_noun = input("choose a plural noun:")
+noun2 = input("choose a noun:")
+place = input("Name of place:")
+adjective= input("choose an adjective(describing word):")
+noun3 = input("choose a noun:")
+
+# print a story from the user input
+print("-----------------------------------")
+print("be kind to your", noun, "-footed", p_noun)
+print("for a duck may be somebody\"s", noun2, ",")
+print("where the weather is always", adjective, ".\n")
+print("you may think that is this the" , noun3, ",")
+print("well it is.")
+print("-------------------------------------------")
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
